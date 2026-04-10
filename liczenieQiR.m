e119371d96b49c4bea034ca889e4fa61638033ce
filipss070 <filipss070@gmail.com>
2026-04-10 22:14:41 +0200

    Update liczenieQiR.m
    poprawione EM(nie jestem pewien funkcji wyliczającej Q) i smoother

--- liczenieQiR.m
+++ liczenieQiR.m
@@ -79,7 +79,7 @@ Psmooth_history = zeros(1, length(x));
 Pcross_history = zeros(1, length(x));
 P_pred_history = zeros(1, length(x));
 
-function [beta, alpha, beta1, innovation, P_out, P_pred_out, LL_out] = KalmanSlidingWindow123(price_eth, price_btc, Q, R, LL_in)
+function [beta, alpha, beta1, innovation, P_out, P_pred_out, LL_out, K_out] = KalmanSlidingWindow123(price_eth, price_btc, Q, R, LL_in)
     
     persistent x P 
      
@@ -108,29 +108,31 @@ function [beta, alpha, beta1, innovation, P_out, P_pred_out, LL_out] = KalmanSli
     beta = x(1);
     alpha = x(2);
     beta1 = x(3);
+    K_out = K;
     P_out = P;
     P_pred_out = P_pred;
     LL_out = LL_in - 0.5*(log(S) + (innovation^2)/S);
 end
 
-function [beta, alpha, beta1, spread, P_filtr,  P_pred_filtr, LL_nowy] = kalman_do_przodu(price_eth, price_btc, Q, R, length)
+function [beta, alpha, beta1, spread, P_filtr,  P_pred_filtr, LL_nowy, K_filtr] = kalman_do_przodu(price_eth, price_btc, Q, R, length)
 
     beta = zeros(1, length);
     alpha = zeros(1, length);
     beta1 = zeros(1, length);
     spread = zeros(1, length);
     P_filtr = {};
+    K_filtr = {};
     P_pred_filtr = {};
     LL_petla = 0;
 
     for i = 1:length
-        [beta(i), alpha(i), beta1(i), spread(i), P_filtr{i}, P_pred_filtr{i}, nowy_LL] = KalmanSlidingWindow123(price_eth(i), price_btc(i), Q, R, LL_petla);
+        [beta(i), alpha(i), beta1(i), spread(i), P_filtr{i}, P_pred_filtr{i}, nowy_LL, K_filtr{i}] = KalmanSlidingWindow123(price_eth(i), price_btc(i), Q, R, LL_petla);
         LL_petla = nowy_LL;
     end
     LL_nowy = LL_petla;
 end
 
-function [beta, alpha, beta1, P, P_cross, spread] = smoother(price_eth, price_btc, beta_in, alpha_in, beta1_in, P_pred_in, P_in, length) 
+function [beta, alpha, beta1, P, P_cross, spread] = smoother(price_eth, price_btc, beta_in, alpha_in, beta1_in, P_pred_in, P_in, K_in, length) 
 
     beta = zeros(1, length);
     alpha = zeros(1, length);
@@ -143,21 +145,21 @@ function [beta, alpha, beta1, P, P_cross, spread] = smoother(price_eth, price_bt
     beta(length) = beta_in(length);
     alpha(length) = alpha_in(length);
     beta1(length) = beta1_in(length);
-    P{length-1} = P_in{length-1};
-    P_cross{length-1} = P_in{length}; %do poprawy, w teorii nieprawda, ale chyba good enough bo to tylko jedna próbka i nie ma znaczącego wpływu na wynik
+    P{length} = P_in{length};
+    P_cross{length} = (eye(3)- K_in{length} * [price_btc(length), 1, 0]) * A * P_in{length}; 
 
-    for i = length-1:-1:2
+    for i = length:-1:2
         C = P_in{i-1} * A' / P_pred_in{i};
-        x = [beta_in(i); alpha_in(i); beta1_in(i)] + C * ([beta(i+1); alpha(i+1); beta1(i+1)] - A * [beta_in(i); alpha_in(i); beta1_in(i)]);
+        x = [beta_in(i-1); alpha_in(i-1); beta1_in(i-1)] + C * ([beta(i); alpha(i); beta1(i)] - A * [beta_in(i-1); alpha_in(i-1); beta1_in(i-1)]);
         P{i-1} = P_in{i-1} + C * (P{i} - P_pred_in{i}) * C';
         H = [price_btc(i), 1, 0];
         spread(i) = price_eth(i) - H * x;
-        beta(i) = x(1);
-        alpha(i) = x(2);
-        beta1(i) = x(3);
+        beta(i-1) = x(1);
+        alpha(i-1) = x(2);
+        beta1(i-1) = x(3);
     end
 
-    for j = length-1:-1:3
+    for j = length:-1:3
         C = P_in{j-1} * A' / P_pred_in{j};
         C1 = P_in{j-2} * A' / P_pred_in{j-1};
         P_cross{j-1} = P_in{j-1}*C1' + C * (P_cross{j} - A*P_pred_in{j-1}) * C1'; 
@@ -173,24 +175,26 @@ function [Q_nowe, R_nowe] = wyliczanie_QR(price_btc, beta, alpha, beta1, P, P_cr
     B1 = zeros(3, 3);
     C1 = zeros(3, 3);
 
-    for i = 3:length-1
+    for i = 3:length
         H = [price_btc(i), 1, 0];
         x = [beta(i); alpha(i); beta1(i)];
         x1 = [beta(i-1); alpha(i-1); beta1(i-1)];
         A1 = A1 + P{i-1} + x1*x1';
         B1 = B1 + P_cross{i} + x*x1';
         C1 = C1 + P{i} + x*x';
-        R = R + (spread(i)^2 + H * P{i} * H')/(length-4);   
+        R = R + (spread(i)^2 + H * P{i} * H')/(length-2);   
     end
 
-    Q_nowe = (C1-(B1/A1)*B1' + (B1/A1 - A)*A1*(B1/A1 - A)')/(length-4);
+    Q_nowe = (C1-(B1/A1)*B1' + (B1/A1 - A)*A1*(B1/A1 - A)')/(length-2);
     R_nowe = R;
 
 end
 
 for krok = 1:100
+    
+    clear KalmanSlidingWindow123;
 
-    [beta_history, alpha_history, beta1_history, spread_history, P_history,  P_pred_history, LL_nowy1] = kalman_do_przodu(y_log, x_log, Q_start, R_start, length(x));
+    [beta_history, alpha_history, beta1_history, spread_history, P_history,  P_pred_history, LL_nowy1, K_EM] = kalman_do_przodu(y_log, x_log, Q_start, R_start, length(x));
 
     roznica = LL_nowy1 - stary_LL;
     if roznica < 0.0001
@@ -200,14 +204,14 @@ for krok = 1:100
     
     stary_LL = LL_nowy1;
 
-    [betasmooth_history, alphasmooth_history, beta1smooth_history, Psmooth_history, Pcross_history, spread_history] = smoother(y_log, x_log, beta_history, alpha_history, beta1_history, P_pred_history, P_history, length(x));
+    [betasmooth_history, alphasmooth_history, beta1smooth_history, Psmooth_history, Pcross_history, spread_history] = smoother(y_log, x_log, beta_history, alpha_history, beta1_history, P_pred_history, P_history, K_EM, length(x));
 
     [Q_start, R_start] = wyliczanie_QR(x_log, betasmooth_history, alphasmooth_history, beta1smooth_history, Psmooth_history, Pcross_history, spread_history, length(x));
 
 end
 
 [beta_history1, alpha_history1, beta1_history1, spread_history1, P_history1,  P_pred_history1, LL_nowy11] = kalman_do_przodu(y_log, x_log, Q_start, R_start, length(x));
-[beta_history2, alpha_history2, beta1_history2, P_pred_history2, Pcross_history2, spread_history2] = smoother(y_log, x_log, beta_history1, alpha_history1, beta1_history1, P_pred_history1, P_history1, length(x));
+[beta_history2, alpha_history2, beta1_history2, P_pred_history2, Pcross_history2, spread_history2] = smoother(y_log, x_log, beta_history1, alpha_history1, beta1_history1, P_pred_history1, P_history1, K_EM, length(x));
 
 XD = y_log - spread_history2';
 subplot(2,1,1); plot(time_axis, y_log, 'b');
